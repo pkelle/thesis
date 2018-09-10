ab305f8347851a6e8a9bd748aaf10f354b40d766
pkelle <patrick.kelle81@gmail.com>
2018-09-10 08:52:04 +0200

    more changes in gdp calc

--- greece.R
+++ greece.R
@@ -1,49 +1,71 @@
 library(MonteCarlo)
 library(plotly)
 library(scales)
+library(tfplot)
 
 i = 1 # mean interest rate
-Y = 194.6 #GDP in Bil. $
+Y = 177735 #GDP in million € for 2017
+Ygrowth2017 = .02
 meanYgrowth = .035
 
 years = c(2017:2030)
 
-
-Ygrowth_vector = rnorm(14, mean=meanYgrowth, sd=.02)
+Ygrowth_vector = rnorm(13, mean=meanYgrowth, sd=.02)
 
 #Ygrowth_vector = percent(Ygrowth_vector, accuracy=0.01 ,scale=1)
-Ygrowth_vector
 
 Y_vector= c(Y)
 
+#calculate GDP up until to 2030
 for(growth in Ygrowth_vector) {
  Y_vector=c(Y_vector, Y_vector[length(Y_vector)] * (1 + growth))
 }
 
-Y_vector=Y_vector[2:length(Y_vector)]
-
-print(Y_vector)  
+Ygrowth_vector=c(Ygrowth2017, Ygrowth_vector)
 
 
-plotdata = data.frame(years, Ygrowth_vector)
-
-ay <- list(
-  tickfont = list(color = "red"),
-  overlaying = "y",
-  side = "right",
-  title = "second y axis"
-)
-
-#p = plot_ly()
-#p = add_lines(p,x = ~1:3, y = ~10*(1:3), name = "slope of 10") 
-#p = add_lines(p,x = ~2:4, y = ~1:3, name = "slope of 1", yaxis = "y2")
-#p = layout(p,   title = "Double Y Axis", yaxis2 = ay,xaxis = list(title="x") )
+#plot GDP and growth
+#ay <- list(
+#  tickfont = list(color = "black"), overlaying = "y", side = "right", title = "GDP in million €"
+#)
+#p =  plot_ly(x = ~years, y = ~Ygrowth_vector, type = 'scatter', mode = 'lines+markers', name = "GDP growth %", line = list(width=0.5) )
+#p = add_trace(p,  y = ~Y_vector,  yaxis = "y2", name ="GDP", line = list(color = 'rgb(205, 12, 24)', width = 2, dash = 'dash'))
+#p = layout(p,title = "GPD prediction", yaxis2 = ay, xaxis = list(title="year"), yaxis=list(title="GDP growth in %"))
+#p
 
 
-p =  plot_ly( x = ~years, y = ~Ygrowth_vector, type = 'scatter', mode = 'lines+markers')
-p = add_trace(p, x=~years, y = ~Y_vector,  yaxis = "y2", line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dash'))
-p = layout(p,title = "Double Y Axis", yaxis2 = ay, xaxis = list(title="x"))
+calculateGrowth <- function(meanY) {
 
+  Ygrowth_vector = rnorm(13, mean=meanY, sd=.02)
+  Y_vector= c(Y)
+  
+  #calculate GDP up until to 2030
+  for(growth in Ygrowth_vector) {
+    Y_vector=c(Y_vector, Y_vector[length(Y_vector)] * (1 + growth))
+  }
+  
+  Ygrowth_vector=c(Ygrowth2017, Ygrowth_vector)
+  
+  return(Y_vector)
+}
+  
+datalist = list()
+for(i in seq(10)) {
+  
+  datalist[[i]] = calculateGrowth(0.035)
+}
+
+dfYgrowth = do.call(rbind, datalist)
+
+YgrowthFinal=colMeans(dfYgrowth)
+
+min(dfYgrowth[,14])
+max(dfYgrowth[,14])
+
+p =  plot_ly(x = ~years, y = ~YgrowthFinal, type = 'scatter', mode = 'lines+markers', name = "GDP growth", line = list(width=0.5) )
 p
+
+Y_ts <- ts(YgrowthFinal, start = c(2017, 1), frequency = 1)
+percentChange(Y_ts)
 
 
