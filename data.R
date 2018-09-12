73580f5f7c5b5f1ae10dfb49521576fa2b3082c8
pkelle <42926975+pkelle@users.noreply.github.com>
2018-09-12 16:23:20 +0200

    Update data.R

--- data.R
+++ data.R
@@ -1,14 +1,14 @@
 
 # https://www.esm.europa.eu/assistance/greece/efsf-programme-greece-expired
-# disbursed 09/03/2012 34.6 billion euro, Amortisation from 2023 to 2042 
+# 1) disbursed 09/03/2012 34.6 billion euro, Amortisation from 2023 to 2042 
+# 2) disbursed 17/12/2012 11.3 billion euro, Amortisation from 2023 to 2042
+# total 45,9 in 20 years yields 2,295 per year
 # the repayment of the loan tranche is equally distributed per year during the repayment period
 # in million euro
-efsm_debt_repayment_installments = ts(c(1730, 1730, 1730, 1730, 1730, 1730, 1730, 1730), start=2023, frequency=1) #up until 2030
+efsm_debt_repayment_installments = ts(c(2295, 2295, 2295, 2295, 2295, 2295, 2295, 2295), start=2023, frequency=1) #up until 2030
 
 # floating rate notes ESM?
 
 # IMF repayment schedule?
-
-# greek banks still lots of nonperforming loans?
 
 
