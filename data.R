bd5d8f0602939e354f4cc647c4ae6fb841ae8954
pkelle <42926975+pkelle@users.noreply.github.com>
2018-09-12 12:42:24 +0200

    Update data.R

--- data.R
+++ data.R
@@ -3,4 +3,12 @@
 # disbursed 09/03/2012 34.6 billion euro, Amortisation from 2023 to 2042 
 # the repayment of the loan tranche is equally distributed per year during the repayment period
 # in million euro
-esm_debt_repayment_installments = ts(c(1730, 1730, 1730, 1730, 1730, 1730, 1730, 1730), start=2023, frequency=1) #up until 2030
+efsm_debt_repayment_installments = ts(c(1730, 1730, 1730, 1730, 1730, 1730, 1730, 1730), start=2023, frequency=1) #up until 2030
+
+# floating rate notes ESM?
+
+# IMF repayment schedule?
+
+# greek banks still lots of nonperforming loans?
+
+
