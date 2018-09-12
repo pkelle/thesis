ffeb39d0e9669c66773544d9803ca377d35ecd4a
pkelle <42926975+pkelle@users.noreply.github.com>
2018-09-12 09:50:15 +0200

    Create data.R
    file for data collecting

---
+++ data.R
@@ -0,0 +1,4 @@
+
+# https://www.esm.europa.eu/assistance/greece/efsf-programme-greece-expired
+# disbursed 09/03/2012 34.6 Amortisation from 2023 to 2042 
+esm_debt_repayment_installments = ts(c(1.73,1.73,1.73,1.73,1.73,1.73,1.73,1.73),start=2023, frequency=1) #up until 2030
