c2b0c2c564d8c66c88c214535230309ab5b4af3b
pkelle <42926975+pkelle@users.noreply.github.com>
2018-09-12 09:55:13 +0200

    Update data.R
    make it million

--- data.R
+++ data.R
@@ -1,4 +1,6 @@
 
 # https://www.esm.europa.eu/assistance/greece/efsf-programme-greece-expired
-# disbursed 09/03/2012 34.6 Amortisation from 2023 to 2042 
-esm_debt_repayment_installments = ts(c(1.73,1.73,1.73,1.73,1.73,1.73,1.73,1.73),start=2023, frequency=1) #up until 2030
+# disbursed 09/03/2012 34.6 billion euro, Amortisation from 2023 to 2042 
+# the repayment of the loan tranche is equally distributed per year during the repayment period
+# in million euro
+esm_debt_repayment_installments = ts(c(1730, 1730, 1730, 1730, 1730, 1730, 1730, 1730), start=2023, frequency=1) #up until 2030
